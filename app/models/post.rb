77cd232889eb8abde8b0175883a8784a19c0e233
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 16:32:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 
   def title_clickbait
     binding.pry
-    title.include?(/("Won't Believe"|"Secret")/)
+    title.include?("Won't Believe")
       #errors[:base] << "title cannot be #{title}"
 
     #elsif title.donwcase.include?("top #{(1..1000)}")
