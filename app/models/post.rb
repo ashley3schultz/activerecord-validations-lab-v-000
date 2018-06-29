a7fe838915ba2944a2b4ac8e2a59217112e519ce
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 16:11:19 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,20 +5,21 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction) }
   validate_with MyValidator
 
-  class Post::MyValidator < ActiveModel::Validator
-    def validate(record)
-      if record.title.donwcase == "won't believe"
-        record.errors[:base] << "title cannot be '#{title}'"
+end
 
-      elsif record.title.donwcase == "secret"
-        record.errors[:base] << "title cannot be '#{title}'"
+class Post::MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title.donwcase == "won't believe"
+      record.errors[:base] << "title cannot be '#{title}'"
 
-      elsif record.title.donwcase == "guess"
-        record.errors[:base] << "title cannot be '#{title}'"
+    elsif record.title.donwcase == "secret"
+      record.errors[:base] << "title cannot be '#{title}'"
 
-      else record.title.donwcase == "top #{(1..1000)}"
-        record.errors[:base] << "title cannot be '#{title}'"
-      end
+    elsif record.title.donwcase == "guess"
+      record.errors[:base] << "title cannot be '#{title}'"
+
+    else record.title.donwcase == "top #{(1..1000)}"
+      record.errors[:base] << "title cannot be '#{title}'"
     end
   end
 end
