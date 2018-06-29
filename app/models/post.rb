f2181f37564dee7f24d47a02706fd590db996ffa
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 15:58:55 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,10 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 100 }
   validates :summary, length: { maximum: 50 }
   validates :category, inclusion: { in: %w(Fiction) }
-  validates :title, exclusion: { in: %w(won't Believe Secret Top Guess)}
+  validate clickbait
 
   def clickbait
+    validates :title, inclusion: { in: %w(won't Believe Secret Top Guess)}
     #validates :title, exclusion: { in: %w(/(won't Believe|Secret|Top|Guess)/)}
     #!title.include?(/(won't Believe|Secret|Top|Guess)/)
   end
