6d58fd12c47e63f7a6324071edae8089bf784c9c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 16:07:00 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,25 @@ class Post < ActiveRecord::Base
   validate_with clickbait
 
   def clickbait
-    validates :title, inclusion: { in: %w(won't Believe Secret Top Guess)}
+    validates :title, inclusion: { in: %w( Secret Top Guess)}
     #validates :title, exclusion: { in: %w(/(won't Believe|Secret|Top|Guess)/)}
     #!title.include?(/(won't Believe|Secret|Top|Guess)/)
   end
 
 end
+
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title.donwcase == "won't believe"
+      record.errors[:base] << "title cannot be 'won't believe'"
+      
+    elsif record.title.donwcase == "secret"
+      record.errors[:base] << "title cannot be 'secret'"
+      
+    elsif record.title.donwcase == "guess"
+      record.errors[:base] << "title cannot be 'guess'"
+    end
+  end
+end
+ 
