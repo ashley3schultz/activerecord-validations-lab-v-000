c6e62a11857b1355b7ff7e5cd70b1c0dba89f0ef
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-29 15:55:26 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   validate :clickbait
 
   def clickbait
-    validates :title, exclusion: { in: %w(/(won't Believe|Secret|Top|Guess)/)
+    validates :title, exclusion: { in: %w(/(won't Believe|Secret|Top|Guess)/)}
     #!title.include?(/(won't Believe|Secret|Top|Guess)/)
   end
 
